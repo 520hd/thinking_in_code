6339f0370782c859d93d5ef67bf8a7b5da62945f
xierishi <xierishi@didiglobal.com>
2020-12-21 11:54:57 +0800

    first commit

---
+++ src/main/java/com/hd520/thinking_in_code/ThinkingInCodeApplication.java
@@ -0,0 +1,13 @@
+package com.hd520.thinking_in_code;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkingInCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkingInCodeApplication.class, args);
+	}
+
+}
